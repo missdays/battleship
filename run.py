4b9729ea50ffc86099b2b4d8356bd69605a44bf2
missdays <dyeneffer13@hotmail.com>
2023-08-23 20:56:38 +0100

    Initial commit, battle between player and computer

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+class BattleShip:
+    """
+    Battle between player and computer.
+    """
+
+    def __init__(self):
+        self.Field_Size = ''
+        self.Ships_Qt = ''
+        self.Ships_Types = []
+        self.Field1 = []
+        self.Field2 = []
